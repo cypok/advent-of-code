f3c978c8c8ae4138bb3e2db127be48a927597d2e
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2025-11-25 08:34:15 +0100

    Year2023-Day17: better data structures, less maps, more arrays
    ~2x performance improvement

--- src/year2023/Day17.kt
+++ src/year2023/Day17.kt
@@ -12,18 +12,29 @@ fun main() = test(
 
 private data class Arrow(val dst: Point, val dir: Dir, val len: Int, val heat: Long, val prev: Arrow?)
 
+private val DIR_COUNT = Dir.entries.size
+
+private class Mark(maxLen: Int) {
+    val heatByDirAndLen = Array(DIR_COUNT * (maxLen + 1)) { Long.MAX_VALUE }
+
+    operator fun get(arr: Arrow): Long {
+        return heatByDirAndLen[arr.dir.ordinal + DIR_COUNT * arr.len]
+    }
+
+    operator fun set(arr: Arrow, heat: Long) {
+        heatByDirAndLen[arr.dir.ordinal + DIR_COUNT * arr.len] = heat
+    }
+}
+
 private fun solve(input: List<String>, minLen: Int, maxLen: Int): Long {
     val heatMap = Array2D.fromLines(input)
 
-    val marksMap = Array2D.of(heatMap.height, heatMap.width) {
-        mutableMapOf<Pair<Dir, Int>, Long>().withDefault { Long.MAX_VALUE }
-    }
+    val marksMap = Array2D.of(heatMap.height, heatMap.width) { Mark(maxLen) }
 
     val queue = PriorityQueue<Arrow>(Comparator.comparing { a -> a.heat })
 
     fun isWorthy(arr: Arrow): Boolean {
-        val marks = marksMap[arr.dst]
-        val prevHeat = marks.getValue(arr.dir to arr.len)
+        val prevHeat = marksMap[arr.dst][arr]
         return arr.heat < prevHeat
     }
 
@@ -46,7 +57,7 @@ private fun solve(input: List<String>, minLen: Int, maxLen: Int): Long {
         val newHeat = arr.heat + heatMap[arr.dst].digitToInt()
 
         if (arr.len >= minLen) {
-            marksMap[arr.dst][arr.dir to arr.len] = arr.heat
+            marksMap[arr.dst][arr] = arr.heat
 
             if (arr.dst.row == heatMap.height - 1 && arr.dst.col == heatMap.width - 1) {
                 return newHeat
